dc214e96e78926813a801ab4723ebbe0cd3c4ef0
Kim <kimberley.quenon.@gmail.com>
2024-02-27 15:14:16 +0100

    first commit - bases

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center h-screen">
+    <div class="container container-home mx-auto max-w-lg p-8 flex items-center justify-center flex-col rounded-xl shadow-2xl">
+      <h1 class="font-bold t-4 mb-4 text-3xl">Contactez-nous</h1>
+      <Form></Form>
+    </div>
+  </div>
+</template>
---
+++ components/Form.vue
@@ -0,0 +1,56 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label>Nom</label>  
+        <input
+          type="text"
+          v-model="name"
+          class="form-control"
+          placeholder="Votre nom...*"
+          required="required"
+        >
+      </div>
+      <div>
+        <label>Prénom</label>  
+        <input
+          type="text"
+          v-model="surname"
+          class="form-control"
+          placeholder="Votre prénom...*"
+          required="required"
+        >
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="email"  
+          v-model="email"
+          class="form-control"
+          placeholder="Votre adresse mail...*"
+          required="required"
+        >
+      </div>
+      <div>
+        <label>Téléphone</label>
+        <input
+          type="tel"  
+          v-model="email"
+          class="form-control"
+          placeholder="Votre numéro de téléphone..."
+        >
+      </div>
+      <div>
+        <label>Message</label>
+        <textarea
+          type="text"
+          v-model="message"
+          class="form-control"
+          placeholder="Message"
+          required="required"
+        ></textarea>
+      </div>
+      <div>
+        <button type="submit" class="btn">Envoyer</button>
+      </div>
+    </form>
+  </template>
