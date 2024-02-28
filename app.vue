806b3d8bca241a70e517021d1ccd909137970168
Kim <kimberley.quenon.@gmail.com>
2024-02-28 09:37:14 +0100

    design tailwind

--- app.vue
+++ app.vue
@@ -6,3 +6,7 @@
     </div>
   </div>
 </template>
+
+<style>
+  @import 'assets/style.css';
+</style>
--- components/Form.vue
+++ components/Form.vue
@@ -54,67 +54,69 @@ const handleSubmit = () => {
 };
 </script>
 <template>
-    <form @submit.prevent="handleSubmit">
-        <div v-if="successMessage" class="bg-green-200 text-green-800 border border-green-600 rounded-md px-4 py-2 mb-4">
-                {{ successMessage }}
-        </div>
-        <div>
-            <label>Nom</label>
-            <input type="text" v-model="formData.name" class="form-control" placeholder="Votre nom...*"
-                @change="v$.name.$touch" :class="{
-                    'border-red-500 focus:border-red-500': v$.name.$error,
-                    'border-[#42d392] ': !v$.name.$invalid,
-                }">
-            <div class="text-xs text-red-500" v-if="v$.name.$error">{{
-                v$.name.$errors[0].$message
-            }}</div>
-        </div>
-        <div>
-            <label>Prénom</label>
-            <input type="text" v-model="formData.surname" class="form-control" placeholder="Votre prénom...*"
-                @change="v$.surname.$touch" :class="{
-                    'border-red-500 focus:border-red-500': v$.surname.$error,
-                    'border-[#42d392] ': !v$.surname.$invalid,
-                }">
-            <div class="text-xs text-red-500" v-if="v$.surname.$error">{{
-                v$.surname.$errors[0].$message
-            }}</div>
-        </div>
-        <div>
-            <label>Email</label>
-            <input type="email" v-model="formData.email" class="form-control" placeholder="Votre adresse mail...*"
-                @change="v$.email.$touch" :class="{
-                    'border-red-500 focus:border-red-500': v$.email.$error,
-                    'border-[#42d392] ': !v$.email.$invalid,
-                }">
-            <div class="text-xs text-red-500" v-if="v$.email.$error">{{
-                v$.email.$errors[0].$message
-            }}</div>
-        </div>
-        <div>
-            <label>Téléphone</label>
-            <input type="tel" v-model="formData.phone" class="form-control" placeholder="Votre numéro de téléphone..."                 
-                @change="v$.phone.$touch" :class="{
-                    'border-red-500 focus:border-red-500': v$.phone.$error,
-                    'border-[#42d392] ': !v$.phone.$invalid,
-                }">
-            <div class="text-xs text-red-500" v-if="v$.phone.$error">{{
-                v$.phone.$errors[0].$message
-            }}</div>
-        </div>
-        <div>
-            <label>Message</label>
-            <textarea type="text" v-model="formData.message" class="form-control" placeholder="Commencez à rédiger..."
-                @change="v$.message.$touch" :class="{
-                    'border-red-500 focus:border-red-500': v$.message.$error,
-                    'border-[#42d392] ': !v$.message.$invalid,
-                }"></textarea>
-            <div class="text-xs text-red-500" v-if="v$.message.$error">{{
-                v$.message.$errors[0].$message
-            }}</div>
-        </div>
-        <div>
-            <button type="submit" class="btn">Envoyer</button>
-        </div>
-    </form>
+    <div class="container mx-auto">
+        <form @submit.prevent="handleSubmit" class="max-w-lg mx-auto">
+            <div v-if="successMessage" class="bg-green-200 text-green-800 border border-green-600 rounded-md px-4 py-2 mb-4">
+                    {{ successMessage }}
+            </div>
+            <div class="mb-4">
+                <label>Nom<span class="text-red-600">*</span></label>
+                <input type="text" v-model="formData.name" class="form-input block w-full border border-gray-300 rounded px-3 py-2" placeholder="Votre nom...*"
+                    @change="v$.name.$touch" :class="{
+                        'border-red-500 focus:border-red-500': v$.name.$error,
+                        'border-[#42d392] ': !v$.name.$invalid,
+                    }">
+                <div class="text-xs text-red-500" v-if="v$.name.$error">{{
+                    v$.name.$errors[0].$message
+                }}</div>
+            </div>
+            <div class="mb-4">
+                <label>Prénom<span class="text-red-600">*</span></label>
+                <input type="text" v-model="formData.surname" class="form-input block w-full border border-gray-300 rounded px-3 py-2" placeholder="Votre prénom...*"
+                    @change="v$.surname.$touch" :class="{
+                        'border-red-500 focus:border-red-500': v$.surname.$error,
+                        'border-[#42d392] ': !v$.surname.$invalid,
+                    }">
+                <div class="text-xs text-red-500" v-if="v$.surname.$error">{{
+                    v$.surname.$errors[0].$message
+                }}</div>
+            </div>
+            <div class="mb-4">
+                <label>Email<span class="text-red-600">*</span></label>
+                <input type="email" v-model="formData.email" class="form-input block w-full border border-gray-300 rounded px-3 py-2" placeholder="Votre adresse mail...*"
+                    @change="v$.email.$touch" :class="{
+                        'border-red-500 focus:border-red-500': v$.email.$error,
+                        'border-[#42d392] ': !v$.email.$invalid,
+                    }">
+                <div class="text-xs text-red-500" v-if="v$.email.$error">{{
+                    v$.email.$errors[0].$message
+                }}</div>
+            </div>
+            <div class="mb-4">
+                <label>Téléphone<span class="text-red-600">*</span></label>
+                <input type="tel" v-model="formData.phone" class="form-input block w-full border border-gray-300 rounded px-3 py-2" placeholder="Votre numéro de téléphone..."                 
+                    @change="v$.phone.$touch" :class="{
+                        'border-red-500 focus:border-red-500': v$.phone.$error,
+                        'border-[#42d392] ': !v$.phone.$invalid,
+                    }">
+                <div class="text-xs text-red-500" v-if="v$.phone.$error">{{
+                    v$.phone.$errors[0].$message
+                }}</div>
+            </div>
+            <div class="mb-4">
+                <label>Message<span class="text-red-600">*</span></label>
+                <textarea type="text" v-model="formData.message" class="form-input block w-full border border-gray-300 rounded px-3 py-2" placeholder="Commencez à rédiger..."
+                    @change="v$.message.$touch" :class="{
+                        'border-red-500 focus:border-red-500': v$.message.$error,
+                        'border-[#42d392] ': !v$.message.$invalid,
+                    }"></textarea>
+                <div class="text-xs text-red-500" v-if="v$.message.$error">{{
+                    v$.message.$errors[0].$message
+                }}</div>
+            </div>
+            <div class="mb-4">
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Envoyer</button>
+            </div>
+        </form>
+    </div>
 </template>
